dfa6f7f5d9c66ce11874426e439e218886e4edd0
Vitalina <vsinkovaa@gmail.com>
2025-04-13 23:59:04 +0300

    hw12

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -28,6 +28,7 @@ export const clearGallery = () => {
 
 export function renderImages(images) {
   const gallery = document.querySelector('.gallery');
+
   gallery.insertAdjacentHTML(
     'beforeend',
     images
@@ -41,59 +42,58 @@ export function renderImages(images) {
           comments,
           downloads,
         }) => `
-          <li class= "gallery-item">
-            <a class="gallery-link" href="${largeImageURL}">
-            <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
-            </a>
-            <div class="info">
-            <p><b>Likes</b><br>${likes}</p>
-            <p><b>Views</b><br>${views}</p>
-            <p><b>Comments</b><br>${comments}</p>
-            <p><b>Downloads</b><br>${downloads}</p>
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+                <p><b>Likes</b><br>${likes}</p>
+                <p><b>Views</b><br>${views}</p>
+                <p><b>Comments</b><br>${comments}</p>
+                <p><b>Downloads</b><br>${downloads}</p>
             </div>
-            </li>`
+      </li>`
       )
       .join('')
-    );
-    lightbox.refresh();
+  );
+
+  lightbox.refresh();
 }
 
 export function initializeLightbox() {
-    if (!lightbox) {
-        lightbox = new SimpleLightbox('.gallery a', {
-            captionsData: 'alt';
-            captionDelay: 250,
-        });
-    }
-    lightbox.refresh();
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
+  lightbox.refresh();
 }
 
 export function smoothScroll() {
-    const firstCard = document.querySelector('.gallery-item');
+  const firstCard = document.querySelector('.gallery-item');
 
-    if (firstCard) {
-        const cardHeight = firstCard.getBoundingClientRect().height;
+  if (firstCard) {
+    const cardHeight = firstCard.getBoundingClientRect().height;
 
-        window.scrollBy({
-            top: cardHeight * 2,
-            left: 0,
-            behavior: "smooth",
-        });
-    }
+    window.scrollBy({
+      top: cardHeight * 2,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
 
 export function showloadMore() {
-    const loadMore = document.querySelector('.load-more');
-
-    if (loadMore) {
-        loadMore.style.display = 'flex';
-    }
+  const loadMore = document.querySelector('.load-more');
+  if (loadMore) {
+    loadMore.style.display = 'flex';
+  }
 }
 
-export function nideloadMore() {
-    const loadMore = document.querySelector('.load-more');
-
-    if (loadMore) {
-        loadMore.style.display = 'none';
-    }
+export function hideloadMore() {
+  const loadMore = document.querySelector('.load-more');
+  if (loadMore) {
+    loadMore.style.display = 'none';
+  }
 }
